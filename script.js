64c54e9c51d819a45c48375b1f6acf5df4724fec
adamgie402 <adamgie402@gmail.com>
2022-09-21 20:46:50 +0200

    sample list generated

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//variables
+const todosList = document.getElementById('todos');
+const input = document.getElementById('inputTodo');
+const addBtn = document.getElementById('add');
+const todos = ['learn JS', 'eat something', 'go outside'];
+
+//events
+window.addEventListener('DOMContentLoaded', loadTodos);
+// addBtn.addEventListener('click', addTodo);
+
+//functions
+function loadTodos() { 
+    // this function load tasks only on startup
+    for (let i = 0; i < todos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = todos[i];
+        todosList.appendChild(li);
+    };
+};
 
