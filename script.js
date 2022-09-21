eaf602a37e1f456d467fb116b8f036895a6a524b
adamgie402 <adamgie402@gmail.com>
2022-09-21 20:56:03 +0200

    task adding done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const todos = ['learn JS', 'eat something', 'go outside'];
 
 //events
 window.addEventListener('DOMContentLoaded', loadTodos);
-// addBtn.addEventListener('click', addTodo);
+addBtn.addEventListener('click', addTodo);
 
 //functions
 function loadTodos() { 
@@ -15,6 +15,30 @@ function loadTodos() {
         let li = document.createElement('li');
         li.textContent = todos[i];
         todosList.appendChild(li);
+        addDelButton(li);
     };
 };
 
+function addDelButton(li){
+    let btn = document.createElement('button');
+    btn.setAttribute("onclick", "removeTodo(this)");
+    btn.textContent = "del";
+    btn.classList.add("delBtn");
+    todosList.appendChild(li);
+    li.appendChild(btn);
+    btn.parentElement.setAttribute("draggable", "true");
+}
+
+function addTodo(e) {
+    // this function adding new task from input
+    e.preventDefault(); // to prevent standard button bevaviour
+    let li = document.createElement('li');
+    li.textContent = input.value    
+    //add to array
+    todos.push(input.value);
+    console.log(todos);
+    //add to screen
+    todosList.appendChild(li);
+    addDelButton(li);
+    input.value = ""; //reset input value in html
+};
