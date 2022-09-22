4cbd891a9cc2331d37fc618b317bf6f18777b295
adamgie402 <adamgie402@gmail.com>
2022-09-22 19:52:55 +0200

    data storing in localstorage done

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 //variables
 const todosList = document.getElementById('todos');
 const input = document.getElementById('inputTodo');
-const addBtn = document.getElementById('add');
-const todos = ['learn JS', 'eat something', 'go outside'];
+const addBtn = document.getElementById('addBtn');
+const delAllBtn = document.getElementById('delAllBtn');
+let todos = [];
+let todosDone = [];
+
 
 //events
-window.addEventListener('DOMContentLoaded', loadTodos);
+window.addEventListener('DOMContentLoaded', getTodos);
 document.getElementById("inputTodo").focus();
 addBtn.addEventListener('click', addTodo);
+delAllBtn.addEventListener('click', deleteAllTodos);
+
+
+function getTodos(){
+    console.log('f getTodos...');
+    // let todos;
+    if(localStorage.getItem('todos') === null){
+        console.log("local storage are empty... add something...");
+        loadDefaultTodos();
+    } else { 
+        todos = JSON.parse(localStorage.getItem('todos'));
+        console.log(todos);
+        showTodos();
+    }
+}
+
+function loadDefaultTodos() {
+    // for testing purpose only...
+    console.log("loading default todos...");
+    const defaultTodos = ['learn JS', 'eat something', 'go outside'];
+    todos = defaultTodos;
+    showTodos();
+} 
+
 
 //functions
-function loadTodos() { 
-    // this function load tasks only on startup
+function showTodos() { 
+    todosList.textContent = "";
     for (let i = 0; i < todos.length; i++){
         let span = document.createElement('span');
         let li = document.createElement('li');
@@ -44,20 +71,12 @@ function addDoneButton(li){
 }
 
 function addTodo(e) {
-    // this function adding new task from input
-    e.preventDefault(); // to prevent standard button bevaviour
-    let span = document.createElement('span');
-    let li = document.createElement('li');
-    span.textContent = input.value;   
-    //add to array
+    console.log('f addtodo...');
+    e.preventDefault(); 
     todos.push(input.value);
     console.log(todos);
-    //add to screen
-    todosList.appendChild(li);
-    li.appendChild(span);
-    addDelButton(li);
-    addDoneButton(li);
     input.value = ""; //reset input value in html
+    saveTodos();
 };
 
 function removeTodo(el){
@@ -70,15 +89,27 @@ function removeTodo(el){
         todos.splice(index, 1);
         console.log(todos);
     };
-    //removing from screen
-    element.remove();
+    saveTodos();
+    getTodos();
 }
 
 function changeStatus(el){
-    
     let element = el.parentElement;
     element = element.firstChild;
     console.log(element);
     element.classList.toggle("taskDone");
-    
+}
+
+function saveTodos(){
+    console.log("f save todos...");
+    localStorage.setItem('todos', JSON.stringify(todos));
+    console.log(JSON.parse(localStorage.getItem('todos')));
+    getTodos();
+};
+
+
+function deleteAllTodos(){
+    console.log('f delalltodos...');
+    todos = [];
+    saveTodos();
 }
